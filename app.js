993e85e7376fc5116f5558cdf3b99a06655d4498
Mateusz Jaworski <mateusz.jaworski@nokia.com>
2015-10-06 00:03:48 +0200

    custom configuration file

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var commander = require('commander');
 
-var config = require('./server.nokia');
+commander
+  .option('-c, --config  <configuration file>', 'configuration file')
+  .parse(process.argv);
+
+var config = require(commander.config || './server.default');
 var routes = require('./server/routes/index')(config);
 
 var app = express();
@@ -26,10 +31,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -37,23 +42,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
